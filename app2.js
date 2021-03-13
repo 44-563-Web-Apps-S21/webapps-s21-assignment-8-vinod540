5d87b9b17f09b1e93c090dfa016ad6946accef53
vinod540 <77802816+vinod540@users.noreply.github.com>
2021-03-13 13:11:31 -0600

    Added  html table response in node.js

--- app2.js
+++ app2.js
@@ -1,7 +1,7 @@
 const http = require('http')    //Pull in a useful node package
                                 
 const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
-const port = process.env.port || 3001               //and the port
+const port = process.env.port || 3000               //and the port
 
 const server =
   http.createServer(            //Creates the response loop
@@ -11,9 +11,39 @@ const server =
 
       res.write('<html> <head> <title> Served </title> </head>')
       res.write('<body>')
-      res.write('Content \n')
-      res.write('More content \n')
-      res.write('Hello World')    
+      res.write('<table><tr><th>Location</th>')
+      res.write('<th>Number Of Hours</th>')
+      res.write('<th>Amount Of Money</th>')
+      res.write('</tr>') 
+      res.write('<tr>')
+      res.write('<td>Tirupati</td>')
+      res.write('<td>Ten</td>')
+      res.write('<td>&#36; 200</td>')
+      res.write('</tr>')
+
+
+      res.write('<tr>')
+      res.write('<td>New delhi</td>')
+      res.write('<td>six</td>')
+      res.write('<td>&#36; 350</td>')
+      res.write('</tr>')
+      
+      
+      res.write('<tr>')
+      res.write('<td>Goa</td>')
+      res.write('<td>Seven</td>')
+      res.write('<td>&#36; 400</td>')
+      res.write('</tr>')
+
+
+      res.write('<tr>')
+      res.write('<td>Kolkata</td>')
+      res.write('<td>Eight</td>')
+      res.write('<td>&#36; 300</td>')
+      res.write('</tr>')        
+      res.write('</table>')  
+      
+      
       res.end('</body></html>')
       //Close the response
     }                           
